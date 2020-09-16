b76c3fb9641342dfa6bc9ea0d3441d27a1ab7d13
ImamHossain-eng <babubpx2762@gmail.com>
2020-09-17 02:19:44 +0600

    Setting up components and fetch data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import Cards from './components/Cards/Cards';
+//import Chart from './components/Chart/Chart';
+//import CountryPicker from './components/CountryPicker/CountryPicker';
+
+import { Cards, Chart, CountryPicker } from './components';
+import style from './App.module.css';
+import { fetchData } from './api';
+
+class App extends React.Component {
+    state = {
+        data: {}
+    }
+
+    async componentDidMount() {
+        const fData = await fetchData();
+        //console.log(data)
+        this.setState({ data: fData })
+    }
+    render() {
+        const { data } = this.state;
+        return (
+            <div className = { style.container } >
+                <Cards data={data}/>
+                <Chart />
+                <CountryPicker />
+            </div>
+        )
+    }
 }
 
 export default App;
